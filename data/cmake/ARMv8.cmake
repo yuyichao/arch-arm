7c64e617cc0cc123ca40159a129959a4e8e98878
Yichao Yu <yyc1992@gmail.com>
2018-09-18 22:19:12 -0400

    [cmake] Set CMAKE_STAGING_PREFIX correctly.
    This override CMAKE_INSTALL_PREFIX to be the installing prefix.
    We want this to be the actual path in the chroot environment rather than
    the path to the chroot in the host.

--- data/cmake/ARMv8.cmake
+++ data/cmake/ARMv8.cmake
@@ -1,7 +1,14 @@
 set(CMAKE_SYSTEM_NAME Linux)
 
 set(CMAKE_SYSROOT /usr/lib/arch-arm-cross/pkg_root/armv8/)
-set(CMAKE_STAGING_PREFIX /usr/lib/arch-arm-cross/pkg_root/armv8/)
+if(NOT DEFINED CMAKE_STAGING_PREFIX)
+  if(DEFINED CMAKE_INSTALL_PREFIX)
+    set(CMAKE_STAGING_PREFIX "${CMAKE_INSTALL_PREFIX}")
+  else()
+    set(CMAKE_STAGING_PREFIX "/usr/local")
+  endif()
+endif()
+set(CMAKE_FIND_NO_INSTALL_PREFIX TRUE)
 
 set(CMAKE_C_COMPILER aarch64-linux-gnu-gcc)
 set(CMAKE_CXX_COMPILER aarch64-linux-gnu-g++)
